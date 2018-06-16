092e1aee1be08fe8be06446789a04292e83cdf01
iPhoenixs <t_huricane@hotmail.com>
2018-06-16 15:32:58 +0700

    Add files via upload

---
+++ lab1.r
@@ -0,0 +1,25 @@
+#1
+#a
+a = c(1:20)
+a
+#b
+b = c(20:1)#sort(sort(x, decreasing = FALSE, ...))
+b
+#c
+c1 = sort(c(1:20),decreasing = FALSE)
+print(c1)
+#d
+tmp = c(4,6,3)
+print(tmp)
+#e
+e = rep(tmp,10)
+print(e)
+?vector
+?rep
+#f
+f = c(rep(tmp,10),rep(tmp,11))
+print(f)
+?sample
+?for
+?crossprod
+
